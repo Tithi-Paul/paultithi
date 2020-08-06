410bbd7676fa7ac89248e00d6d35c6f5c288976d
Tithi-Paul <tithi.cse3.bu@gmail.com>
2020-08-06 13:47:25 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+	$('.menu-toggler').on('click', function () {
+		$(this).toggleClass('open');
+		$('.top-nav').toggleClass('open');
+	});
+
+	$('.top-nav .nav-link').on('click', function () {
+		$('.menu-toggler').removeClass('open');
+		$('.top-nav').removeClass('open');
+	});
+
+	$('nav a[href*="#"]').on('click', function () {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top - 100
+		}, 2000);
+	})
+
+	$(window).scroll(function(){
+    if($(this).scrollTop() > 40){
+      $('#up').fadeIn();
+    } else{
+      $('#up').fadeOut();
+    }
+  });
+
+  $("#up").click(function(){
+    $('html ,body').animate({scrollTop : 0},800);
+  });
+
+  AOS.init({
+  	easing: 'ease',
+  	duration: 1800
+  });
+
+});
+
+
+  
